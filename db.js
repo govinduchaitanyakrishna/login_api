220b35b8e03344c55497b5f668f7d74e82b7ebc2
“GitHub_user_name_here” <govinduchaitanya3@gmail.com>
2021-12-08 17:25:11 +0530

    code

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://document:rk123456@cluster0.gfit3.mongodb.net/eduaug?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://govinduchaitanya3:1234@cluster0.jmt1h.mongodb.net/loginapi?retryWrites=true&w=majority');
